f8885ec42ca160ab2876e43673f31554466975fc
matejgerek <maftvej@gmail.com>
2023-02-24 13:27:39 +0100

    feat: add console.log

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-console.log('test performance');
+import semanticRelease from "semantic-release";
+
+const run = async () => {
+    const result = await semanticRelease(
+        {
+            dryRun: true,
+            plugins: [
+
+                "@semantic-release/release-notes-generator"
+            ],
+            // Core options
+            branches: [
+                "main",
+                "staging",
+            ],
+            repositoryUrl: "https://github.com/matejgerek/test-repo.git"
+        },
+        {
+            /*env: { ...process.env, MY_ENV_VAR: "MY_ENV_VAR_VALUE" },*/
+        }
+    )
+
+    console.log(result)
+    console.log('staging')
+}
+
+run()
