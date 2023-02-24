18eca621d4078c733f7fd60de59032c237f407d4
matejgerek <maftvej@gmail.com>
2023-02-24 14:11:42 +0100

    fix: add semanticRelease js api

--- .github/workflows/getRcDescriptionAndNewTag/index.js
+++ .github/workflows/getRcDescriptionAndNewTag/index.js
@@ -1,12 +1,20 @@
 import {exec} from "child_process";
 import {promisify} from "util";
+import semanticRelease from "semantic-release";
 
 const execAsync = promisify(exec);
 
 const getRcDescriptionAndNewTagWithSemanticRelease = async () => {
     try {
-        const { stdout } = await execAsync("npx semantic-release");
-        console.log(stdout);
+        const result = semanticRelease({
+            dryRun: true,
+            plugins: [
+                "@semantic-release/commit-analyzer",
+                "@semantic-release/release-notes-generator",
+                ],
+        });
+
+        console.log(result);
     } catch (error) {
         console.log(error);
     }
